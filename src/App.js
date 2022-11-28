85d1bb8bd1c6f0614c06e3bfd16b544fd505d315
mels-tovmasyan <99186717+mels-tovmasyan@users.noreply.github.com>
2022-11-28 19:10:03 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://t.me/comradepetrov"
+          href="Ari tuuuuuuuuuuuuuuun!"
           target="_blank"
           rel="noopener noreferrer"
         >
