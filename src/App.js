3845a443db4a70d5ae0ff80957ef5eb2b5a994b2
mels-tovmasyan <99186717+mels-tovmasyan@users.noreply.github.com>
2022-11-28 10:27:48 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://t.me/comradepetrov"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Contact me
         </a>
       </header>
     </div>
