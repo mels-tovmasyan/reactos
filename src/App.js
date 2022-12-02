7602c9858cb5e824415916f76616e6913cfd2e3f
mels-tovmasyan <tovmasyan.mels@gmail.com>
2022-12-02 10:09:53 +0400

    changes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,48 @@
-import logo from './logo.svg';
+import image from './image.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://t.me/comradepetrov"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Contact me
-        </a>
-      </header>
+      <aside class="profile-card">
+        <header>
+          <a href="https://t.me/ComradePetrov">
+            <img src={image} class="hoverZoomLink" />
+          </a>
+          <h1>
+            Mels Tovmasyan
+          </h1>
+          <h2>
+            Systems Engineer
+          </h2>
+
+        </header>
+        <div class="profile-bio">
+          <p>
+            Skilled DevOps Engineer with 3+ years of hands-on experience supporting, automating, and optimizing
+            mission critical deployments in AWS, leveraging configuration management, CI/CD, and DevOps processes.
+          </p>
+        </div>
+
+        <ul class="profile-social-links">
+          <li>
+            <a class="zoom" target="_blank" href="https://t.me/ComradePetrov">
+              <i class="fa fa-telegram"></i>
+            </a>
+          </li>
+          <li>
+            <a class="zoom" target="_blank" href="https://www.linkedin.com/in/mels-tovmasyan/">
+
+              <i class="fa fa-linkedin"></i>
+            </a>
+          </li>
+          <li>
+            <a class="zoom" target="_blank" href="https://github.com/mels-tovmasyan/">
+              <i class="fa fa-github"></i>
+            </a>
+          </li>
+        </ul>
+      </aside>
     </div>
   );
 }
